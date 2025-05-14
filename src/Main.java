afa72114f600bbb9a143077a5f39e218ce8dd706
EguchiSoraki <241089@std.hi-joho.ac.jp>
2025-05-14 10:36:25 +0900

    テスト！

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
         // IntelliJ IDEA によるその修正案を確認できます。
         System.out.printf("江口　空哉");
+        System.out.println("ファイルに変更を加えた");
+
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
---
+++ src/kadai0.java
@@ -0,0 +1,5 @@
+public class kadai0 {
+    public static void main(String[] args) {
+        System.out.println("aaaaiu");
+    }
+}
